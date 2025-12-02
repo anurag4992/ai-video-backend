b0805c9206cc37837d97be4a95b2c2dba0cf2c36
anurag4992 <67609750+anurag4992@users.noreply.github.com>
2025-12-02 18:06:42 +0530

    index update

--- index.js
+++ index.js
@@ -36,8 +36,11 @@ app.post("/process", (req, res) => {
     status: "Processing done",
     outputUrl: "https://dummy.ai/output.mp4"
   });
-});
-
+})
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server running on port", PORT));
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on PORT:", PORT);
+  });
+  
 
